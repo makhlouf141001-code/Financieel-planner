bc57c907ce167056b51a714d604268b832b419fd
makhlouf141001-code <makhlouf14.10.01@gmail.com>
2026-02-19 00:29:48 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "fp-cache-v1";
+const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k))))).then(() => self.clients.claim())
+  );
+});
+self.addEventListener("fetch", (e) => {
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((c) => c.put(req, copy)).catch(()=>{});
+      return res;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
